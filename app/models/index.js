f3e0224654396c6a706f0807009864767c417355
pnkhanh <pnkhanh@pnkhanhs-MacBook-Pro.local>
2026-03-16 16:35:09 +0700

    SSL Sequelize DB

--- app/models/index.js
+++ app/models/index.js
@@ -1,17 +1,25 @@
 import dbConfig from "../config/db.config.js";
 import Sequelize from "sequelize";
 import Tutorial from "./tutorial.model.js";
- 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD,{
     host: dbConfig.HOST,
+    port: dbConfig.PORT,
     dialect: dbConfig.dialect,
     pool: dbConfig.pool,
-    port: dbConfig.PORT,
-});
- 
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
+  }
+);
+
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 db.tutorials = Tutorial(sequelize, Sequelize);
- 
+
 export default db;
