23dbd1bb3bfc15be984dd27d541b316e4066a41d
pnkhanh <pnkhanh@pnkhanhs-MacBook-Pro.local>
2026-03-16 16:55:27 +0700

    Final version for deploying in Azure Devops to Azure App Service and Azure Static Web App

--- server.js
+++ server.js
@@ -1,38 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import db from "./app/models/index.js";
-// import tutorialRoutes from "./app/routes/tutorial.routes.js";
- 
-// const app = express();
- 
-// const corsOptions = {
-//     origin: "http://localhost:5173",
-// };
- 
-// app.use(cors(corsOptions));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
- 
-// // Simple route
-// app.get("/", (req, res) => {
-//     res.json({ message: "Welcome to the Tutorial Application." });
-// });
- 
-// // Routes
-// tutorialRoutes(app);
- 
-// // Sync database
-// db.sequelize.sync().then(() => {
-//     console.log("Synced db.");
-// });
- 
-// const PORT = process.env.PORT || 8080;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}.`);
-// });
-
-//--------------------------------------------------
-
 import express from "express";
 import cors from "cors";
 import db from "./app/models/index.js";
@@ -73,23 +38,3 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-//--------------------------------------------------------------
-
-// import express from "express";
-
-// const app = express();
-
-// app.get("/", (req,res)=>{
-//  res.send("App running");
-// });
-
-// app.get("/health",(req,res)=>{
-//  res.json({status:"ok"});
-// });
-
-// const PORT = process.env.PORT || 8080;
-
-// app.listen(PORT,"0.0.0.0",()=>{
-//  console.log("Server running",PORT);
-// });
