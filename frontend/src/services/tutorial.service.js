da6a6394f8a58246913e5c0a43197360b46a63d1
pnkhanh <pnkhanh@pnkhanhs-MacBook-Pro.local>
2026-04-06 17:48:18 +0700

    Change api base url

--- frontend/src/services/tutorial.service.js
+++ frontend/src/services/tutorial.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://api.abcmntxyz.online/";
+  import.meta.env.VITE_API_BASE_URL || "https://api.abcmntxyz.online/api";
 
 const http = axios.create({
   baseURL: API_BASE_URL,
